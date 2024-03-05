328fad68b788de0c947302951c2d562fb956d070
lahcenassmira <lahcenassmira@gmail.com>
2024-03-04 18:19:23 -0800

    Added loading animation

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@
 const openM = document.querySelector("#mobile");
 const closeM = document.querySelector("#close");
 const showContainer = document.querySelector(".container");
+const logoR = document.querySelector("#logo");
+
+const scrollButton = document.querySelector('.scroll');
+logoR.addEventListener("click", () => location.href = "index.html");
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) { // Adjust 100 to the desired scroll position
+    scrollButton.style.display = 'none';
+
+
+  }
+  else {
+    scrollButton.style.display = 'block';
+  }
+});
+
+logoR.addEventListener("click", () => location.href = "index.html");
 
 
 openM.addEventListener("click", () => {
@@ -20,6 +36,18 @@ closeM.addEventListener("click", () => {
 
 
 
+//scroll blur
+// Listen to the scroll event
+window.addEventListener('scroll', function () {
+
+  var header = document.querySelector('header');
+  header.style.backdropFilter = 'blur(' + (window.scrollY / 50) + 'px)';
+  header.style.boxShadow = "0 3px 4px #061423";
+  if (window.scrollY < 110) {
+    header.style.boxShadow = "none";
+  }
+
+});
 
 
 
@@ -34,7 +62,7 @@ closeM.addEventListener("click", () => {
 
 // animation 
 
-var duration = 15 * 2000;
+var duration = 15 * 1000;
 var animationEnd = Date.now() + duration;
 var skew = 1;
 
@@ -76,7 +104,7 @@ const headerAnimation = anime({
   translateY: [-50, 0],
   opacity: [0, 1],
   easing: 'easeInOutQuad', // Easing function
-  duration: 1000, // Duration of animation in milliseconds
+  duration: 3500, // Duration of animation in milliseconds
   delay: 200, // Delay before animation starts in milliseconds
   loop: false // Animation does not loop
 
@@ -86,7 +114,7 @@ const lahcenImg = anime({
   opacity: [0, 1],
   translateX: ['30%', '0%'],
   easing: 'easeInOutQuad',
-  duration: 1500, // Duration of animation in milliseconds
+  duration: 3500, // Duration of animation in milliseconds
   delay: 400, // Delay before animation starts in milliseconds
   loop: false // Animation does not loop
 });
@@ -95,7 +123,7 @@ const lahcenTxt = anime({
   opacity: [0, 1],
   translateX: ['0%', '10%'],
   easing: 'easeInOutQuad',
-  duration: 1500, // Duration of animation in milliseconds
+  duration: 3500, // Duration of animation in milliseconds
   delay: 400, // Delay before animation starts in milliseconds
   loop: false // Animation does not loop
 });
@@ -104,7 +132,7 @@ const lahcenSocial = anime({
   opacity: [0, 1],
   translateY: ['30%', '0%'],
   easing: 'easeInOutQuad',
-  duration: 1500, // Duration of animation in milliseconds
+  duration: 3500, // Duration of animation in milliseconds
   delay: 400, // Delay before animation starts in milliseconds
   loop: false // Animation does not loop
 });
@@ -113,7 +141,7 @@ const lahcenLogo = anime({
   translateY: [-50, 0],
   opacity: [0, 1],
   easing: 'easeInOutQuad', // Easing function
-  duration: 1000, // Duration of animation in milliseconds
+  duration: 3500, // Duration of animation in milliseconds
   delay: 200, // Delay before animation starts in milliseconds
   loop: false// Animation does not loop
 });
@@ -122,7 +150,7 @@ const lahcenMobile = anime({
   translateX: [-90, 0],
   opacity: [0, 1],
   easing: 'easeInOutQuad', // Easing function
-  duration: 1000, // Duration of animation in milliseconds
+  duration: 3500, // Duration of animation in milliseconds
   delay: 200, // Delay before animation starts in milliseconds
   loop: false// Animation does not loop
 });
@@ -137,7 +165,30 @@ window.onload = function () {
 };
 
 //Scroll
-const btn = document.getElementById('scrollBtn');
-btn.addEventListener('click', function() {
-  document.querySelector('.about').scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest"});
+const btn = document.querySelector('.scroll');
+btn.addEventListener('click', function () {
+  document.querySelector('.about').scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Simulate an API request or any async operation
+  setTimeout(() => {
+    hideLoader();
+    showContent();
+    
+  }, 2000); // Replace with your actual data loading logic and time
+
+  function hideLoader() {
+    const loader = document.getElementById("loader");
+  
+    loader.style.display = "none";
+  
+  }
+
+  function showContent() {
+    const content = document.getElementById("content");
+    content.style.display = "block";
+  
+  }
 });
